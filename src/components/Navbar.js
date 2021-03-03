a3f5f30db2d221c8c4abab02c7f3142e5dc0fd57
Agwa <agwa.dan@gmail.com>
2021-03-03 12:46:39 +0300

    Update logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
-import logo from '../paw-print.svg'
+import { Link } from 'react-router-dom';
+import logo from '../telephone.svg'
 import styled from 'styled-components'
-import {ButtonStyler} from './Button';
-import { MdShoppingCart} from 'react-icons/md';
-import {IconContext} from 'react-icons';
-
-
-
+import { ButtonStyler } from './Button';
+import { MdShoppingCart } from 'react-icons/md';
+import { IconContext } from 'react-icons';
 
 export default class Navbar extends Component {
     render() {
@@ -17,39 +14,39 @@ export default class Navbar extends Component {
 https://www.iconfinder.com/icons/1243689/call_phone_icon
 Creative Commons (Attribution 3.0 Unported);
 https://www.iconfinder.com/Makoto_msk */
-}
-            <Link to='/'>
-                <img src={logo} alt="store" className="navbar-brand" />
-            </Link>
+                }
+                <Link to='/'>
+                    <img src={logo} alt="store" className="navbar-brand" />
+                </Link>
 
-            <ul className="navbar-nav align-items-center">
-                <li className="nav-item ml-5">
-                    <Link to="/" className="nav-link">merchandise</Link>
-                </li>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link">merchandise</Link>
+                    </li>
 
-                <li className="nav-item ml-5">
-                    <Link to='/About' className="nav-link">About</Link>
-                </li>
-            </ul>
+                    <li className="nav-item ml-5">
+                        <Link to='/About' className="nav-link">About</Link>
+                    </li>
+                </ul>
 
-            <Link to='/Cart' className="ml-auto">
+                <Link to='/Cart' className="ml-auto">
 
-               <IconContext.Provider
-                value = {{
-                    color: "white",
-                    size: 32
-                }}
-               > 
-              <MdShoppingCart/>
-              </IconContext.Provider>
-              
-            </Link>
-            </NavStyler>       
+                    <IconContext.Provider
+                        value={{
+                            color: "white",
+                            size: 32
+                        }}
+                    >
+                        <MdShoppingCart />
+                    </IconContext.Provider>
+
+                </Link>
+            </NavStyler>
         )
     }
 }
 
-const NavStyler = styled.nav `
+const NavStyler = styled.nav`
     background-color: var(--mainBrown)!important;
     .nav-link{
         color: var(--mainWhite)!important;
