ca3f91c9635ac6c49806637a89fa1995bd584cd4
Agwa <agwa.dan@gmail.com>
2021-03-04 12:21:08 +0300

    Add build

---
+++ build/precache-manifest.56ce16c202ada36f61e3c8e521e86c09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696d4f64f04f9270dd72c2e72aa8e962",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acd63fb9f26c7052328b",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "af0e87a67189300395b5",
+    "url": "/static/css/main.06d2c86d.chunk.css"
+  },
+  {
+    "revision": "acd63fb9f26c7052328b",
+    "url": "/static/js/2.8e6e9158.chunk.js"
+  },
+  {
+    "revision": "af0e87a67189300395b5",
+    "url": "/static/js/main.89454ad3.chunk.js"
+  },
+  {
+    "revision": "0f52cc303f53f663a51b",
+    "url": "/static/js/runtime~main.481637a9.js"
+  },
+  {
+    "revision": "d376c439106a5bb80efc0ed16b1788a7",
+    "url": "/static/media/telephone.d376c439.svg"
+  }
+]);
